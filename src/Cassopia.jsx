8794094f9ee508c279ad0a5b33e20049c892c230
amde-88 <amde-88@gmail.com>
2024-12-26 02:27:13 -0800

    Initial commit for cassopia-app

---
+++ src/Cassopia.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Cassopia = () => {
+  return (
+    <div className="background flex items-center justify-center">
+      <h1 className="text-white text-3xl">Welcome to Cassopia</h1>
+    </div>
+  );
+};
+
+export default Cassopia;
